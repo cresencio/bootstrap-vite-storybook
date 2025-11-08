f87a76ab5aa26b9cc9a93a2c6c21b8af5d34903a
cresencio <cresenciocantu@gmail.com>
2025-11-08 11:30:17 -0600

    Add comments to clarify Bootstrap JS imports in Storybook and main files

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,5 +1,7 @@
 import type { Preview } from '@storybook/react-vite'
 import '../src/styles/bootstrap-custom.scss'
+// Import Bootstrap JS so data-bs attributes work in Storybook
+import 'bootstrap/dist/js/bootstrap.bundle'
 
 const preview: Preview = {
   parameters: {
--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,8 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.scss'
+// Import Bootstrap JS so data-bs attributes (collapse, accordion, etc.) work
+import 'bootstrap/dist/js/bootstrap.bundle';
 import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
