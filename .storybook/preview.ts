ffd558ccf6cd51f36d8e0da18877908f8c41c1af
cresencio <cresenciocantu@gmail.com>
2025-11-08 10:13:17 -0600

    first commit

---
+++ .storybook/preview.ts
@@ -0,0 +1,15 @@
+import type { Preview } from '@storybook/react-vite'
+import '../src/styles/bootstrap-custom.scss'
+
+const preview: Preview = {
+  parameters: {
+    controls: {
+      matchers: {
+        color: /(background|color)$/i,
+        date: /Date$/i,
+      },
+    },
+  },
+};
+
+export default preview;
---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div className="container mt-5">
+      <h1>Bootstrap + Vite + Storybook</h1>
+      <p className="lead">Your app starts here.</p>
+    </div>
+  )
+}
+
+export default App
